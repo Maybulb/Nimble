40d2cb8e411aef3134d743f83b4ed6d5a03d2f26
Josh Trommel <joshtrommel@gmail.com>
2015-12-11 19:58:44 -0800

    Got requesting working, but it's messy and doesn't work well

--- static/js/request.js
+++
@@ -1,8 +0,0 @@
-var request = require('request');
-var util = require('util');
-baseURL = "https://nimble-backend.herokuapp.com/input?i=%s";
-
-var sendRequest = function(query) {
-  queryURL = util.format(baseURL, query)
-  console.log("URL: " + queryURL); // boo, i'm going to sleep
-}
--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,25 @@
 var $ = require('jquery');
+var request = require('request');
+var util = require('util');
 
+var baseURL = "https://nimble-backend.herokuapp.com/input?i=%s";
+
+// clean this miserable piece of shit up
 $(document).keypress(function(e) {
   if (e.which === 13) {
-    alert('query: ' + $('#input').val());
+    var encodedQuery = encodeURIComponent($('#input').val());
+    var queryURL = util.format(baseURL, encodedQuery);
+
+    alert(queryURL);
+
+    // boo, request
+    request(queryURL, function(err, res, body) {
+      if (!err && res.statusCode == 200) {
+        alert(body);
+      } else {
+        alert('Error parsing: ' + queryURL);
+      }
+    })
+
   }
 });
