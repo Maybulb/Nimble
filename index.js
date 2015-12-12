bc320bf789a55a460949dd38fe69fcba78045e2f
Josh Trommel <joshtrommel@gmail.com>
2015-12-12 00:07:33 -0800

    Moved index into static but it seems to be loading slightly slower now

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ var mb = menubar({
 	height:55,
 	width: 380,
 	icon: './static/img/menubar_icon.png',
+	index: 'file://' + __dirname + '/static/index.html'
 });
 
 mb.on('after-create-window', function() {
-	// mb.tray.setPressedImage('./static/img/menubar_icon_pressed.png');
 	// mb.window.openDevTools()
+	mb.tray.setPressedImage('./static/img/menubar_icon_pressed.png');
 	mb.window.setResizable(false);
 })
 
 mb.on('ready', function ready ()  {
 	console.log('Nimble is ready');
 })
-
-mb.height = 1000;
