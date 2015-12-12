e3ab911117c62533cba584bd7778d134695288e4
Josh Trommel <joshtrommel@gmail.com>
2015-12-11 21:44:02 -0800

    Response is working now. Improving now too

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,11 @@ var $ = require('jquery');
 var request = require('request');
 var util = require('util');
 
+function get_type(thing){
+    if(thing===null)return "[object Null]"; // special case
+    return Object.prototype.toString.call(thing);
+}
+
 var baseURL = "https://nimble-backend.herokuapp.com/input?i=%s";
 
 // clean this miserable piece of shit up
@@ -10,12 +15,11 @@ $(document).keypress(function(e) {
     var encodedQuery = encodeURIComponent($('#input').val());
     var queryURL = util.format(baseURL, encodedQuery);
 
-    alert(queryURL);
-
-    // boo, request
+    // this is messy af. fix it!!!!!!!!!
     request(queryURL, function(err, res, body) {
       if (!err && res.statusCode == 200) {
-        alert(body);
+        var json = JSON.parse(body);
+        alert("\"" + json.result.input + "\" : " + json.result.result.plaintext);
       } else {
         alert('Error parsing: ' + queryURL);
       }
