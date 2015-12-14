956f66ab2848f6fc48cf15b242fdbbf46eee179c
Adam Lewes <addm.lwws@gmail.com>
2015-12-14 00:57:18 +0000

    WOOOO

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var mb = menubar({
 });
 
 mb.on('after-create-window', function() {
-	// mb.window.openDevTools()
+  // mb.window.openDevTools()
 	mb.window.setResizable(false);
 	mb.tray.setPressedImage('./static/img/menubar_icon_pressed.png');
 })
