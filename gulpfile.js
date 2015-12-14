47e7e7a4be97687bb4261b481461e7ed775a8efe
Josh Trommel <joshtrommel@gmail.com>
2015-12-13 19:05:24 -0800

    src, instead

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-	return gulp.src('static/css/*.sass')
+	return gulp.src('src/css/*.sass')
 		.pipe(sass())
-		.pipe(gulp.dest('static/css'))
+		.pipe(gulp.dest('src/css'))
 })
 
 gulp.task('compile', ['sass']);
 
 gulp.task('watch', function() {
-	gulp.watch('static/css/*.sass', ['sass'])
+	gulp.watch('src/css/*.sass', ['sass'])
 })
 
 gulp.task('default', ['compile']);
--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var menubar = require('menubar');
 var mb = menubar({
 	height: 50,
 	width: 380,
-	icon: './static/img/menubar_icon.png',
-	index: 'file://' + __dirname + '/static/index.html'
+	icon: './src/img/menubar_icon.png',
+	index: 'file://' + __dirname + '/src/index.html'
 });
 
 mb.on('after-create-window', function() {
   // mb.window.openDevTools()
 	mb.window.setResizable(false);
-	mb.tray.setPressedImage('./static/img/menubar_icon_pressed.png');
+	mb.tray.setPressedImage('./src/img/menubar_icon_pressed.png');
 })
 
 mb.on('ready', function ready ()  {
