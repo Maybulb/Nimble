02a924e902aa748a99cc5793fde1a911362b152a
Josh Trommel <joshtrommel@gmail.com>
2015-12-15 23:25:53 -0800

    I added progress but it doesn't actually work yet

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ var mb = menubar({
 });
 
 mb.on('after-create-window', function() {
-  // mb.window.openDevTools()
-  mb.window.setResizable(false);
+  mb.window.openDevTools()
+  // mb.window.setResizable(false);
 	mb.tray.setPressedImage('./src/img/menubar_icon_pressed.png');
 })
 
