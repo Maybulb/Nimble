7c4a6e2e4a61e4283208109d4a0e67679ab30075
Josh Trommel <joshtrommel@gmail.com>
2015-12-18 14:59:18 -0800

    Indentation

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var mb = menubar({
 mb.on('after-create-window', function() {
   // mb.window.openDevTools()
   mb.window.setResizable(false);
-	mb.tray.setPressedImage('./src/img/menubar_icon_pressed.png');
+  mb.tray.setPressedImage('./src/img/menubar_icon_pressed.png');
 })
 
 mb.on('ready', function ready ()  {
