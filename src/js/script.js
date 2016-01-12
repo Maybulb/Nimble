2a6217cc0f629aeb29ccbd59b52ef8b30c63aae6
Ethan Arterberry <ethanarterberry@gmail.com>
2016-01-12 20:57:33 +0000

    add mathjs code, not tested. @trmml or @gthn please test it. should work tho

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,7 @@ var $ = require('jquery'),
     ipcRenderer = electron.ipcRenderer,
     request = require('request'),
     progress = require('request-progress'),
+    math = require("mathjs"),
     URL = "https://nimble-backend.herokuapp.com/input?i=%s";
 
 function resizeWindow() {
@@ -14,11 +15,11 @@ function resizeWindow() {
 
 $(document).ready(function() {
   $.getJSON('js/suggestions.json', function(json) {
-    var placeholder = rand.paul(json)
+    var placeholder = rand.paul(json);
     $('#input').attr('placeholder', placeholder);
     console.log('Placeholder set to: ' + placeholder);
-  })
-})
+  });
+});
 
 $(document).keypress(function(event) {
   if (event.which === 13) query();
@@ -27,21 +28,36 @@ $(document).keypress(function(event) {
 var query = function () {
   var encodedQuery = encodeURIComponent($('#input').val());
   var queryURL = util.format(URL, encodedQuery);
-
-  console.log('Queried with: ' + queryURL)
-
-  progress(request(queryURL))
-  .on('progress', function(state) {
-    console.log('progress: ' + state.percent) // Not working at all ¯\_(ツ)_/¯
-  })
-  .on('data', function(data) {
-    var json = JSON.parse(data);
-    var plaintext = json.result.result.plaintext;
-    $(".output").text(plaintext)
+  var result;
+  
+  // try to evaluate w/ mathjs
+  try {
+    result = math.eval($('#input').val());
+    
+    $(".output").text(result);
     resizeWindow();
-  })
-  .on('error', function(err) {
-    console.log('Error:' + err)
-  })
+  } catch(e) {
+    // if input isn't math throw error and use wolfram code
+    console.log("Input is not math. Using Wolfram|Alpha. If you'd like, the error message given by MathJS is as follows:\n" + e);
+    
+    progress(request(queryURL))
+    .on('progress', function(state) {
+      console.log('progress: ' + state.percent); // Not working at all ¯\_(ツ)_/¯
+    })
+    .on('data', function(data) {
+      var json = JSON.parse(data);
+      result = json.result.result.plaintext;
+      
+      $(".output").text(result);
+      resizeWindow();
+    })
+    .on('error', function(err) {
+      console.log('Error:' + err);
+    });
+  }
+
+  console.log('Queried with: ' + queryURL);
+
+  
 
 }
