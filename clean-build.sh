01bfc51886e76b49a3c9c8e28dfb3fd0ffc3d1e8
Jacob Marshall <git@jacobmarshall.co>
2016-05-31 18:24:15 +1200

    Add a clean-build.sh file for building releases

---
+++ clean-build.sh
@@ -0,0 +1,14 @@
+# clean build
+node_version=$(node -v)
+echo $node_version
+n 4.4.5
+node -v
+git pull
+rm -rf node_modules/
+npm install
+gulp sass
+gulp package
+codesign --deep --force --verbose --sign "$CODESIGN_KEY" Nimble-darwin-x64/Nimble.app
+codesign --verify -vvvv Nimble-darwin-x64/Nimble.app
+gulp build
+n "$node_version"
